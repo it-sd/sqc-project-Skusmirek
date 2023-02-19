15a2d047b07d631643b829aaa0fe5249002657e6
Skusmirek <skusmirek@student.cvtc.edu>
2023-02-18 20:32:51 -0600

    Routes commit

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotnev').config()
+const express = require('express')
+const path = require('path')
+const PORT = process.env.port || 5163
+
+express().use(express.static(path.join(__dirname, 'public'))).use(express.json()).use(express.urlencoded({ extended: true})).set('views', path.join(__dirname, 'views')).set('view engine', 'ejs')
+
+.get('health', function (req, res) {
+  res.status(200).send('Healthy')
+})
+
+.get('/', function (req, res){
+  res.render('pages/index')
+})
+
+.get('/about', function (req, res) {
+  res.render('pages/about')
+})
