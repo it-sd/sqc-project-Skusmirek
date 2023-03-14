c06570bc7442f58df1d77633ea9b29f3b15778df
Skusmirek <skusmirek@student.cvtc.edu>
2023-03-13 22:15:27 -0500

    Trying to fix javascript to work on render

--- spec/tests.js
+++ spec/tests.js
@@ -1,7 +1,7 @@
-const { createQuiz } = require('.../');
+const { createQuiz } = require('../server.js');
 
 
-// Define a mock quiz with one question and answer
+
 const mockQuiz = [
   {
     question: "What is the capital of France?",
@@ -14,28 +14,21 @@ const mockQuiz = [
   }
 ];
 
-// Define a mock answer for the mock quiz
 const mockAnswer = "Paris";
 
 describe("createQuiz", function() {
   beforeEach(function() {
-    // Set up the mock prompt function to return the mock answer
     spyOn(window, "prompt").and.returnValue(mockAnswer);
   });
 
   it("should prompt the user for an answer for each quiz question", function() {
-    // Call the createQuiz function with the mock quiz
     createQuiz(mockQuiz);
-
-    // Expect the prompt function to have been called once for each quiz question
     expect(window.prompt.calls.count()).toEqual(mockQuiz.length);
   });
 
   it("should calculate the user's score correctly", function() {
-    // Call the createQuiz function with the mock quiz
+    
     createQuiz(mockQuiz);
-
-    // Expect the user's score to be 1 (since the mock answer is correct)
     expect(window.alert).toHaveBeenCalledWith(`Your score: 1 out of ${mockQuiz.length}`);
   });
 });
